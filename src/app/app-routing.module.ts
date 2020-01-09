89dab15cdf4ebda95fcc164f515b93adaab7c402
Raymond Lui <phoenixc0622@gmail.com>
2020-01-09 07:08:57 -0500

    Set up routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+  },
+]
 
 @NgModule({
   imports: [
-    CommonModule
+    RouterModule.forRoot(routes)
   ],
+  exports: [ RouterModule ],
   declarations: []
 })
 export class AppRoutingModule { }
