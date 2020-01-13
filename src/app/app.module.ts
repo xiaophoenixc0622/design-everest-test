56dc4fbfbc0bc9cea310e1dce75a6ac9bc1f3579
Raymond Lui <phoenixc0622@gmail.com>
2020-01-13 03:46:46 -0500

    Add bootstrap in the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 
 @NgModule({
@@ -13,8 +15,10 @@ import { AppRoutingModule } from './/app-routing.module';
     DashboardComponent
   ],
   imports: [
+    FontAwesomeModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
